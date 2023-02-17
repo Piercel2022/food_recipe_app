88305c06a4908bfdecd9b521f2e56fff7aad8d05
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-02-17 15:12:28 +0100

    Revert "Merge pull request #15 from Piercel2022/recipe_food"
    This reverts commit d0763d5b89b71a9a0da8e887ea4a98d79205d12c, reversing
    changes made to 24c1ecb3d4045871931854633952ca33909bd8f3.

--- app/models/recipe_food.rb
+++
@@ -1,5 +0,0 @@
-class RecipeFood < ApplicationRecord
-  belongs_to :recipe
-  belongs_to :food
-  validates :food, uniqueness: { scope: :recipe, message: "You can't add the same recipe for the same food" }
-end
--- db/migrate/20230214150243_create_foods.rb
+++ db/migrate/20230214150243_create_foods.rb
@@ -1,11 +1,9 @@
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
-      t.references :user, null: false, foreign_key: true
       t.string :name
-      t.string :measurement_unit
-      t.integer :price
-      t.integer :quantity
+      t.string :measuring_unit
+      t.float :price
 
       t.timestamps
     end
