30ac013f4ad0f01d72f3640d8f9e8e8042c747f8
onomeh <onwuanibeonome@gmail.com>
2023-02-18 15:14:22 +0000

    Modify delete functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :foods
-  has_many :recipes
+  has_many :foods, dependent: :destroy
+  has_many :recipes, dependent: :destroy
 
   validates :name, presence: true
+
 end
